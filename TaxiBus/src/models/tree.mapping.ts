3cf80419fa736e32aeeb38843a4b461e04ae9a0f
unknown <cornet.thomas017@gmail.com>
2017-11-13 10:29:26 -0500

    css page home

--- TaxiBus/src/models/tree.mapping.ts
+++ TaxiBus/src/models/tree.mapping.ts
@@ -6,16 +6,55 @@ export interface Treemap{
 }
 
 export const TreeMappingList=[
+
     {
         'name':'Hotel de Ville',
         'numero':'422',
         lat:45.255050,
         lng:-74.131829
     },
+
     {
         'name':'St Thomas / Du Marché',
         'numero':'408',
         lat:45.254680,
         lng:-74.133154
+    },
+    {
+        'name':'80 rue rue Marché',
+        'numero':'402',
+        lat:45.253738,
+        lng:-74.135487
+    },
+    {
+        'name':'Parc Sauvé / Nicholson',
+        'numero':'420',
+        lat:45.256483,
+        lng:-74.131879
+    },
+    {
+        'name':'18 rue de la Fabrique',
+        'numero':'400',
+        lat:45.255708,
+        lng:-74.136234
+    },
+    {
+        'name':'CEGEP',
+        'numero':'404',
+        lat:45.253246,
+        lng:-74.133725
+    },
+    {
+        'name':'Champlin / Wilson',
+        'numero':'430',
+        lat:45.255591,
+        lng:-74.128267
+    },
+    {
+        'name':'Bibliothéque Municipale',
+        'numero':'410',
+        lat:45.253429,
+        lng:-74.132092
     }
+
 ]
